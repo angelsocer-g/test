c96479bcf5c7170d9cbdef31a70c07b0ee28d51d
djay.yim <angelsocer@gmail.com>
2021-08-23 00:12:04 +0900

    first commit

---
+++ src/test/java/github/angelsocer/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package github.angelsocer.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
